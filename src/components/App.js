ee1e6de698b96f5a71a101a9997370a9c38c75de
Julia <ska-keda@yandex.ru>
2020-12-18 12:54:46 +0300

    init

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ST from './index.scss'
+
+
+class App extends React.Component {
+
+    render(){
+        return(
+            <div className={ST.wrapper}>
+                Hi
+            </div>
+        )
+    }
+}
+
+export default App
